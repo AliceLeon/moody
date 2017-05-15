58efc0e72270836e8dc9a4483ffcf5e7ae6f4b0b
AliceLeon <aliceleon_@outlook.com>
2017-05-16 04:22:26 +0800

    可以显示一组数据了 可喜可贺

--- usedata.js
+++ usedata.js
@@ -1,9 +1,31 @@
 /**
  * Created by Alice on 2017/5/14.
  */
-var alice = "04-14 19:13"
-
+var alice = "04-14 2:59"
+var nono = "2016-7-26 00:55"
 // 时间格式转化
+var real_radius = 0
+function radius_angle(data,start_year,end_year,start_r,end_r){
+    var r_range = end_r - start_r
+    // console.log("r_range"+r_range)
+    var year_range = end_year-start_year+1
+    // console.log("year_range"+year_range)
+    var temp_data = time_date(data);
+    // console.log("temp_data"+temp_data)
+    var now_year = temp_data.tyear
+    // console.log("now_year"+now_year)
+    var day_in_year = get_radius(temp_data.tyear, temp_data.tmonth, temp_data.tday)
+    // console.log("day_in_year"+day_in_year)
+    var real_radius = ((parseFloat(now_year)+parseFloat(day_in_year)-parseFloat(start_year))/parseFloat(year_range))*parseFloat(r_range)+parseFloat(start_r)
+    // console.log("real_radius"+real_radius)
+
+    return {
+        "return_radius" : real_radius,
+        "return_angle" : get_angle(temp_data.thour, temp_data.tminute)
+    }
+}
+radius_angle(nono,2015,2017,100,200)
+
 function time_date (data){
     var date = data.split(" ")[0].split("-")
     var time = data.split(" ")[1].split(":")
@@ -16,14 +38,8 @@ function time_date (data){
     }
 }
 
-//
 
-//获得情绪值
-var emotionurl = "https://api.prprpr.me/emotion/wenzhi?password=DIYgod&text={一句话}"
-var datatest = "小拳拳捶你胸口"
-
-
-var get_radius = function(year,month,date){
+function get_radius(year,month,date){
 
     var monthdays = 0
     var is366 = 0
@@ -40,7 +56,7 @@ var get_radius = function(year,month,date){
         default:
             total = 365
     }
-    console.log("total: "+ total)
+    // console.log("total: "+ total)
     //关于月份的处理
     function add_month(a_month) {
         switch (parseInt(a_month)) {
@@ -72,19 +88,35 @@ var get_radius = function(year,month,date){
 
     for(var j = 1;j< month ;j++){
         monthdays += add_month(j)
-        console.log("直到第"+j+"月有"+ monthdays+"天")
+        // console.log("直到第"+j+"月有"+ monthdays+"天")
     }
-    console.log(monthdays)
-    console.log(date)
+    // console.log(monthdays)
+    // console.log(date)
     var month_day = parseInt(monthdays) + parseInt(date) //类型转换 哭哭
-    console.log("天数："+month_day)
-    console.log()
+    // console.log("天数："+month_day)
+    // console.log()
     return (month_day/total)
 }
 
-var get_angle = function(anhour,anminute){
-    return (anhour-1)/24+ parseInt(anminute)/60
+function get_angle(a,b){
+    a = a-1
+    // console.log('a'+a)
+    a = a/24
+    // console.log('A'+a)
+    b = b/60/24
+    // console.log('b'+b)
+
+    return (a+b)<0?0:(a+b);
 }
-console.log("qqqq"+get_angle(1,2))
+console.log("time_date(alice).thour "+time_date(alice).thour)
+console.log("time_date(alice).tminute "+time_date(alice).tminute)
+console.log("qqqq: "+get_angle(time_date(alice).thour,time_date(alice).tminute))
+console.log("qqqq: "+get_angle(1,2))
+console.log("qqqq: "+get_angle(19,17))
+console.log("qqqq: "+get_angle(23,59))
+console.log("qqqq: "+get_angle(13,00))
 // console.log("angle: "+ get_angle(time_date(alice).thour,time_date(alice).tminute))
 console.log(get_radius(time_date(alice).tyear,time_date(alice).tmonth,time_date(alice).tday))
+var testtemp = radius_angle(alice)
+console.log(testtemp.return_angle)
+console.log(testtemp.return_radius)
