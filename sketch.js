97dff636905535f6459baa3bf947eeb861bb7680
AliceLeon <aliceleon_@outlook.com>
2017-05-18 02:56:55 +0800

    2:56

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
 // 绘画心情点的函数
-
-function real_draw(real_r,an,r_fill,small_r,a_text){  // 半径，与x轴偏离角度
-    var an = TWO_PI*an
-    var a = width/2 + sin(an)* real_r //半径可变成呼吸灯
-    var b = height/2 - cos(an)* real_r
-    var c = small_r*10
-    noStroke()
-    fill(255,Math.floor(r_fill*255)); // 此处可变
-    ellipse(a,b,c,c);
-
-}
+//
+// function real_draw(real_r,an,r_fill,small_r,a_text){  // 半径，与x轴偏离角度
+//     var an = TWO_PI*an
+//     // var a = width/2 + sin(an)* real_r //半径可变成呼吸灯
+//     // var b = height/2 - cos(an)* real_r
+//     var a = width/2+sin(an)* real_r //半径可变成呼吸灯
+//     var b = height/2+-cos(an)* real_r
+//     var c = small_r*10
+//     noStroke()
+//     fill(255,Math.floor(r_fill*255)); // 此处可变
+//     ellipse(a,b,c,c);
+//
+// }
